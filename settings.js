8b9e8f04c2d9b151fd5b4c3c4095015683e13135
lanceajiro <142672718+lanceajiro@users.noreply.github.com>
2025-10-30 06:35:42 +0000

    Update

--- settings.js
+++ settings.js
@@ -1,17 +1,31 @@
 module.exports = {
-    name: 'Shin APIs',
-    description: 'This interactive interface allows you to explore and test our comprehensive collection of API endpoints in real-time.',
-    icon: '/docs/image/icon.png',
-    author: 'ShinDesu',
-    telegram: 'https://t.me/+AQO22J2q6KBlNWM1',
-    notification: [ 
-        {
-            title: 'New API Added',
-            message: 'Blue Achieve and Loli API have been added to the documentation.',
-        },
-        {
-            title: 'System Update',
-            message: 'The API documentation system has been updated to version 0.0.2'
-        }
-    ]
+  name: "Shin APIs",
+  description:
+    "This interactive interface allows you to explore and test our comprehensive collection of API endpoints in real-time.",
+  header: {
+    status: "Online!",
+    imageSrc: [
+      "https://cdnb.artstation.com/p/assets/images/images/066/149/065/original/killer-rabbit-media-chinese-alleyway-export.gif?1692171513",
+    ],
+    imageSize: {
+      mobile: "80%",
+      tablet: "40%",
+      desktop: "40%",
+    },
+  },
+  icon: "/docs/image/icon.png",
+  author: "ShinDesu",
+  telegram: "https://t.me/+AQO22J2q6KBlNWM1",
+  notification: [
+    {
+      title: "New API Added",
+      message:
+        "Blue Achieve and Loli API have been added to the documentation.",
+    },
+    {
+      title: "System Update",
+      message:
+        "The API documentation system has been updated to version 0.0.2.",
+    },
+  ],
 };
